2f9c425a347d0622fedf2256dd3167564690ca6e
Raphael Roberts <raphael.roberts48@gmail.com>
2021-04-14 19:37:51 -0500

    Updated to python3; run black; fix change in nltk

--- madman.py
+++ madman.py
@@ -1,26 +1,39 @@
+from builtins import range
+from builtins import object
 import nltk
-from nltk.tag.simplify import simplify_wsj_tag
+from nltk.tag import pos_tag, map_tag
 import random
 import Markov
+
 
 def clean_word(word):
     """Get the junk off a word."""
     return word.strip().strip(".,'!?\"()*;:-")
 
+
 def is_punctuation(word):
-    return (clean_word(word) == "")
+    return clean_word(word) == ""
+
 
 def filter_pos(words, pos):
     """Words is a dict of word: count pairs, and pos a NLTK tag.
 
     Non-matching words are set to have a count of 0, rather than being removed.
     """
-    wn_parts_map = {"N": "n", "ADV": "r", "ADJ": "s", "NP": "n", "V": "v",
-                    "VD": "v", "VG": "v", "VN": "v"}
-    
+    wn_parts_map = {
+        "NOUN": "n",
+        "ADV": "r",
+        "ADJ": "s",
+        "NP": "n",
+        "VERB": "v",
+        "VD": "v",
+        "VG": "v",
+        "VN": "v",
+    }
+
     pos = wn_parts_map[pos]
 
-    for word in words.iterkeys():
+    for word in words.keys():
         syns = nltk.corpus.wordnet.synsets(clean_word(word))
         right_pos = False
         for syn in syns:
@@ -32,9 +45,10 @@ def filter_pos(words, pos):
 
     return words
 
+
 class Madman(object):
     # Only these parts of speech may be replaced with madlibs.
-    allowed_parts = {"ADJ", "ADV", "N", "NP", "V", "VD", "VG", "VN"}
+    allowed_parts = {"ADJ", "ADV", "NOUN", "NP", "VERB", "VD", "VG", "VN"}
 
     # Do not replace these words.
     skiplist = {"are", "is", "was"}
@@ -43,7 +57,7 @@ class Madman(object):
 
     def __init__(self, brain, cacheDir="cache/"):
         self.cache = cacheDir
-        
+
         self.load(brain)
 
     def load(self, brain):
@@ -51,17 +65,22 @@ class Madman(object):
 
     def madlib(self, text):
         """Take a sentence and madlibify it, returning the result text."""
-
-        tagged_text = nltk.pos_tag(nltk.tokenize.word_tokenize(text))
-        simplified = [(word, nltk.tag.simplify.simplify_wsj_tag(tag))
-                      for word, tag in tagged_text]
+        token_text = nltk.tokenize.word_tokenize(text)
+        tagged_text = pos_tag(token_text)
+        simplified = [
+            (word, map_tag("en-ptb", "universal", tag)) for word, tag in tagged_text
+        ]
+        print(simplified)
 
         new_text = [simplified[0][0], simplified[1][0]]
         for i in range(2, len(simplified)):
             word, pos = simplified[i]
-            if pos in self.allowed_parts and word not in self.skiplist and \
-               random.random() <= self.madlib_prob:
-               word = self.replace_word(word, pos, simplified[i-2:i])
+            if (
+                pos in self.allowed_parts
+                and word not in self.skiplist
+                and random.random() <= self.madlib_prob
+            ):
+                word = self.replace_word(word, pos, simplified[i - 2 : i])
 
             new_text.append(word)
 
@@ -77,21 +96,19 @@ class Madman(object):
     def replace_word(self, word, pos, context):
         """Devise a fun replacement for a word using two words of context."""
 
-        words = filter_pos(self.brain.getNextWords([context[0][0],
-                                                    context[1][0]]),
-                           pos)
+        words = filter_pos(self.brain.getNextWords([context[0][0], context[1][0]]), pos)
 
         if len(words) == 0:
             return word
 
         weightedWords = dict()
-        for word, num in words.iteritems():
+        for word, num in words.items():
             weightedWords[word] = num
-            
+
         # do a weighted random selection from the words
         rand = random.randint(0, sum(weightedWords.values()))
         pos = 0
-        for word, weight in weightedWords.items():
+        for word, weight in list(weightedWords.items()):
             pos += weight
             if rand <= pos:
                 return word
--- makeChains.py
+++ makeChains.py
@@ -3,8 +3,10 @@
 Generate Markov chains to be cached in the filesystem; this saves us the effort
 of generating new chains every time we want to make poetry.
 """
+from __future__ import print_function
 
 import sys, Markov, os
+
 
 def makeChain(name, sourceDir, cacheDir):
     """Parse the source text to generate new Markov chains."""
@@ -18,30 +20,34 @@ def makeChain(name, sourceDir, cacheDir):
             if len(words) > 0:
                 fwd.add(words)
     except IOError:
-        print "The source named '%s' does not exist" % (name,)
+        print("The source named '%s' does not exist" % (name,))
     finally:
         fwd.close()
 
+
 def deleteChainCache(name, cacheDir):
     """Delete the old cached versions of the chains, so we start fresh rather
-       than adding to the previous chain files."""
+    than adding to the previous chain files."""
     try:
         os.remove(cacheDir + name + "-fwd")
     except OSError:
         # if they don't exist, we don't care
         pass
 
-if __name__ == "__main__":         
+
+if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print """Usage:
+        print(
+            """Usage:
     makeChains.py chainName
 where chainName is a specific source text to parse and cache."""
+        )
         sys.exit(1)
-    
-    print "Deleting old cache files"
+
+    print("Deleting old cache files")
     deleteChainCache(sys.argv[1], "cache/")
 
-    print "Creating new cache files"
+    print("Creating new cache files")
     makeChain(sys.argv[1], "sources/", "cache/")
 
-    print "Chain(s) created successfully."
+    print("Chain(s) created successfully.")
